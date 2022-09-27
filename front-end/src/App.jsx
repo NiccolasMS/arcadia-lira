c8b800f6947f3c65be480bf9c627e7ff32e31a45
HannaSantos <leonardo.salomao@bandtec.com.br>
2022-09-26 21:13:56 -0300

    tentando fazer header.

---
+++ front-end/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Box } from "@mui/material";
+import Logo from "../assets/logo.svg";
+import Header from "./Components/Header";
+
+function App() {
+  return (
+    <Box>
+      <Header />
+    </Box>
+  );
+}
+
+export default App;
