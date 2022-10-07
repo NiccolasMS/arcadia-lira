12b01b61bcd809c67a76d98f2dd803f8ae3e4b51
brunohenriquechagas <BRUNO.CALMEIDA@bandtec.com.br>
2022-10-07 15:57:07 -0300

    classes com tratamento de falhas finalizada

--- back-end/Lira v2/Arcadia/src/main/java/Lira/Arcadia/controle/repositorio/EncomendaRepository.java
+++ back-end/Lira v2/Arcadia/src/main/java/Lira/Arcadia/controle/repositorio/EncomendaRepository.java
@@ -1,8 +1,2 @@
-package Lira.Arcadia.controle.repositorio;
-
-import Lira.Arcadia.controle.controle.EncomendaController;
-import Lira.Arcadia.controle.dominio.Encomenda;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EncomendaRepository extends JpaRepository<Encomenda, Integer> {
+package Lira.Arcadia.controle.repositorio;public interface EncomendaRepository {
 }
