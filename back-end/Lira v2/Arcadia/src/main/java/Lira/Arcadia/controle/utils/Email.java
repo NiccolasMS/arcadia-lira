14447235ef4c84f8f242f62263dbd29296913cfe
brunohenriquechagas <BRUNO.CALMEIDA@bandtec.com.br>
2022-11-23 19:49:02 -0300

    javaMail consertado

--- back-end/Lira v2/Arcadia/src/main/java/Lira/Arcadia/controle/utils/Email.java
+++ back-end/Lira v2/Arcadia/src/main/java/Lira/Arcadia/controle/utils/Email.java
@@ -25,29 +25,41 @@ public class Email{
         //props.setProperty("mail.smtp.ssl.trust", "smtp.gmail.com");
 
 
+        System.out.println("Inicio da session");
+
         Session session = Session.getInstance(props, new javax.mail.Authenticator(){
-                protected PasswordAuthentication getPasswordAuthentication()
-                {
-                    return new PasswordAuthentication(usuario,senha);
-                }
+            protected PasswordAuthentication getPasswordAuthentication()
+            {
+                return new PasswordAuthentication(usuario,senha);
+            }
         });
+
+        System.out.println("Iniciando get properties");
 
         session.getProperties().put("mail.smtp.starttls.enable", "true");
         session.getProperties().put("mail.smtp.ssl.trust", "smtp.office365.com");
 
+        System.out.println("Iniciando debug");
+
         session.setDebug(true);
 
+        System.out.println("Debug true iniciando");
+
         try{
+            System.out.println("Instanciando objeto");
             Message message = new MimeMessage(session);
             message.setFrom(new InternetAddress(usuario));
 
             Address[] toUser = InternetAddress.parse("bruno.calmeida@bandtec.com.br");
-            message.setRecipients(Message.RecipientType.TO,InternetAddress.parse("thiago.cordeiro@bandtec.com.br"));
+            System.out.println("Criando conteudo da mensagem");
+            message.setRecipients(Message.RecipientType.TO,InternetAddress.parse("brunoo_henriquee_chagas@hotmail.com"));
             message.setSubject("Stephanini demissão");
             message.setText("Agradecemos pelo seus serviços, está demitido");
+            System.out.println("Pronto para enviar...");
             Transport.send(message);
         }catch (Exception e)
         {
+            System.out.println("Deu erro dentro do try");
             System.out.println(e);
         }
 
