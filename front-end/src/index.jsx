01b139cd5e53cc85486da435c590fba95908ea0f
brunohenriquechagas <BRUNO.CALMEIDA@bandtec.com.br>
2023-03-11 20:09:46 -0300

    atualizando

--- front-end/src/index.jsx
+++ front-end/src/index.jsx
@@ -1,6 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Router from "./routers/routes";
+
+import {React} from "react";
+import {ReactDOM} from "react-dom";
+import {Router} from "./routers/routes";
+
 
 ReactDOM.render(
   <React.StrictMode>
